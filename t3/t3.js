e68a51b57fbe87c74028c6fe3d93fc538c118e28
Deni <mrthelink09@gmail.com>
2025-09-01 13:02:55 +0300

    t3 tehty

--- t3/t3.js
+++ t3/t3.js
@@ -28,16 +28,38 @@ const todoList = [
   },
 ];
 
-// haetaan ul
-const ul = document.querySelector("ul");
+function displayInfo() {
+  const now = new Date();
+  const target = document.getElementById("target");
 
-// käydään läpi listaa
-todoList.forEach(item => {
-  const checked = item.completed ? "checked" : "";
-  ul.insertAdjacentHTML("beforeend", `
-    <li>
-      <input type="checkbox" id="todo-${item.id}" ${checked}>
-      <label for="todo-${item.id}">${item.task}</label>
-    </li>
-  `);
-});
+  const browser = navigator.userAgent;
+  const os = navigator.platform;
+
+  target.innerHTML = `
+    <p><b>Browser info:</b> ${browser}</p>
+    <p><b>Operating System:</b> ${os}</p>
+    <p><b>Screen size:</b> ${screen.width} x ${screen.height}</p>
+    <p><b>Available space:</b> ${screen.availWidth} x ${screen.availHeight}</p>
+    <p><b>Date:</b> ${now.toLocaleDateString("fi-FI", {
+      day: "numeric",
+      month: "long",
+      year: "numeric"
+    })}</p>
+    <p><b>Time:</b> ${now.toLocaleTimeString("fi-FI", {
+      hour: "2-digit",
+      minute: "2-digit"
+    })}</p>
+  `;
+}
+
+function displayTodo() {
+  const todoUl = document.getElementById("todo");
+  todoList.forEach(item => {
+    const li = document.createElement("li");
+    li.textContent = `${item.task} ${item.completed ? "✅" : "❌"}`;
+    todoUl.appendChild(li);
+  });
+}
+
+displayInfo();
+displayTodo();
